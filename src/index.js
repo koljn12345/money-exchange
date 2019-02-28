2a3562571696046cd748a26cfe4479e2c16681a0
koljn12345 <litovchenko_kolj@mail.ru>
2019-03-01 01:47:30 +0300

    easy money

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+   let nominals= {"H":50,"Q":25,"D":10,"N":5,"P":1};
+   let quantityBills= {};
+   if(currency>10000) { quantityBills= {error: "You are rich, my friend! We don't have so much coins for exchange"};}
+   else {
+      for(let nominal in nominals) {
+         let count=currency/nominals[nominal];
+         if(count>=1) {
+            quantityBills[nominal]=(count)|0;
+            currency-=nominals[nominal]*quantityBills[nominal];
+         }
+      }
+   }
+ return quantityBills;
 }
